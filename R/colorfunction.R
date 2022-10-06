0528132324e4eb3f4a40d835257aef2b78d2398e
Arthur Timmermans <arthur.timmermans@student.hu.nl>
2022-10-06 12:28:38 +0100

    added help documentations

--- R/colorfunction.R
+++ R/colorfunction.R
@@ -1,5 +1,16 @@
-#base function:
-
+#' costum color package base function
+#'
+#' @param palette choose a pellet from the 'colorlist'
+#' @param alpha sets the transparency
+#' @param reverse if set to true, the order of the colorlist is reversed
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' show_col(blackwhite_pal("blackwhite")(7))
+#' filled.contour(volcano,color.palette = blackwhite_pal("blackwhite"), asp=1)
+#'
 blackwhite_pal <- function(palette="blackwhite", alpha = 1, reverse = FALSE) {
   pal <- colorlist[[palette]]
   if (reverse){
@@ -9,10 +20,27 @@ blackwhite_pal <- function(palette="blackwhite", alpha = 1, reverse = FALSE) {
 }
 
 
-#color palette:
-color_pallete_blackwhite <- function(..., palette = "blackwhite",
-                                      discrete = TRUE, alpha = 1,
-                                      reverse = FALSE) {
+
+#' color palette for ggplot2
+#'
+#' @param ... allows for additional arguments to scale_color_gradient, is not a requirement to fill in
+#' @param palette choose a pellet from the 'colorlist'
+#' @param discrete allows to choose a discrete color palette or not
+#' @param alpha sets the transparency
+#' @param reverse if set to true, the order of the colorlist is reversed
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' ggplot(mtcars, aes(hp, mpg)) +
+#' geom_point(aes(color = factor(carb)), size = 4) +
+#'  color_pallete_blackwhite()
+#'
+#'
+ color_pallete_blackwhite <- function(..., palette = "blackwhite",
+                                       discrete = TRUE, alpha = 1,
+                                     reverse = FALSE) {
   if (discrete) {
     discrete_scale("color", "colorlist", palette=blackwhite_pal(palette, alpha = alpha, reverse = reverse))
   } else {
@@ -20,7 +48,20 @@ color_pallete_blackwhite <- function(..., palette = "blackwhite",
   }
 }
 
-#fill palette:
+#' fill palette
+#'
+#' @param ... allows for additional arguments to scale_color_gradient, is not a requirement to fill in
+#' @param palette choose a pellet from the 'colorlist'
+#' @param discrete allows to choose a discrete color palette or not
+#' @param alpha sets the transparency
+#' @param reverse if set to true, the order of the colorlist is reversed
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' ggplot(mtcars) + geom_bar(aes(x = hp, fill = mpg)) + fill_pallete_blackwhite()
+#'
 fill_pallete_blackwhite <- function(..., palette="blackwhite",
                                      discrete = TRUE, alpha=1, reverse = TRUE) {
   if (discrete) {
